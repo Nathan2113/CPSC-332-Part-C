8eb17911dddb384747e37c87a160fd17a3671fb1
Noah Sanderson <Noah4u76@gmail.com>
2025-12-10 23:33:39 -0800

    Fixed assets

--- includes/header.php
+++ includes/header.php
@@ -7,17 +7,17 @@ require_once __DIR__ . '/functions.php';
 <head>
     <meta charset="UTF-8">
     <title>Movie Theatre</title>
-    <link rel="stylesheet" href="/movie-theatre/assets/styles.css">
+    <link rel="stylesheet" href="/assets/styles.css">
 </head>
 <body>
 <header>
     <h1>Movie Theatre</h1>
     <nav>
-        <a href="/movie-theatre/public/index.php">Home</a>
-        <a href="/movie-theatre/public/movies.php">Movies</a>
-        <a href="/movie-theatre/public/showtimes.php">Showtimes</a>
-        <a href="/movie-theatre/public/my_tickets.php">My Tickets</a>
-        <a href="/movie-theatre/public/reports.php">Reports</a>
+        <a href="/index.php">Home</a>
+        <a href="/movies.php">Movies</a>
+        <a href="/showtimes.php">Showtimes</a>
+        <a href="/my_tickets.php">My Tickets</a>
+        <a href="/reports.php">Reports</a>
     </nav>
 </header>
 <main>
